150ef2efb6f17c1ffbf46c499da12948f3a458f1
Rafael Fernandes <faelsfernandes@gmail.com>
2019-09-12 22:43:00 -0300

    FutureBuilder adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,10 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance?key=71e89b16";
 
 void main() async {
-
   print(await getData());
 
-
   runApp(MaterialApp(
-    home: Home(
-
-    ),
+    home: Home(),
   ));
 }
 
@@ -28,16 +24,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("\$ Conversor de dinheiros \$"),
+        title: Text(
+          "\$ Conversor de dinheiros \$",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 25.0, color: Colors.black54),
+        ),
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
-//      body: ,
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Carregando dados...",
+                    style: TextStyle(
+                        color: Colors.amber,
+                    fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                    child: Text("Erro ao carregar dados :( ",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }else{
+                  return Container(color: Colors.lightGreen,);
+                }
+            }
+          }),
     );
   }
 }
+
+//theme: ThemeData(
+//inputDecorationTheme: InputDecorationTheme(
+//enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+//)
+//)
